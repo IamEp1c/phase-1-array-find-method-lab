9d384dce7ff2dfa18d5576d9e2271f12561e051c
Waqas Anjum <Qanjum96@gmail.com>
2022-04-10 18:08:21 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// passing in the array here
+function superbowlWin(record){
+ const records = record.find(recordIterator => recordIterator.result === 'W')
+    if(records){
+        return records.year
+    } else {
+        return undefined
+    }
+    // need to make an if else statement
+ }
+
+// superbowlWin(record); // passing in array 
